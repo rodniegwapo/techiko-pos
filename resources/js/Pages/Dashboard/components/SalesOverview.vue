d27790df9d5ac47392b858336073542398f83f6d
rodniegwapo <rodnie.lecera@gmail.com>
2025-10-04 15:00:19 +0800

    update: fixed graph

--- resources/js/Pages/Dashboard/components/SalesOverview.vue
+++ resources/js/Pages/Dashboard/components/SalesOverview.vue
@@ -13,9 +13,9 @@ const graphFilter = defineModel("graphFilter");
         <div class="flex items-center justify-between mb-4">
             <h3 class="text-lg font-semibold text-gray-900">Sales Overview</h3>
             <a-radio-group v-model:value="graphFilter" button-style="solid">
+                <a-radio-button value="daily">Daily</a-radio-button>
                 <a-radio-button value="weekly">Weekly</a-radio-button>
                 <a-radio-button value="monthly">Monthly</a-radio-button>
-                <a-radio-button value="yearly">Yearly</a-radio-button>
             </a-radio-group>
         </div>
         <VueApexCharts
