c974c77cbbda7e099d63e55c20523a2c2b634706
rodniegwapo <rodnie.lecera@gmail.com>
2026-04-14 13:55:52 +0800

    update

--- database/seeders/ProductSoldTypeSeeder.php
+++ database/seeders/ProductSoldTypeSeeder.php
@@ -13,8 +13,8 @@ class ProductSoldTypeSeeder extends Seeder
      */
     public function run(): void
     {
-         $types = [
-            ['name' => 'Biece', 'created_at' => now(), 'updated_at' => now()],
+        $types = [
+            ['name' => 'Piece', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Box', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Pack', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Dozen', 'created_at' => now(), 'updated_at' => now()],
